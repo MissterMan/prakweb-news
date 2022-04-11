ebc8c1ac3b887428c27ea29ce6230152ef53ea8d
Pratama Putra Prayogi <putrayogi4545@gmail.com>
2022-04-11 16:49:11 +0700

    Fixing dropdown

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ toogleHamburger.addEventListener("click", function () {
   const mobileView = document.querySelector("#mobile__dropdown");
   mobileView.classList.toggle("block");
 });
+
+const toogleDrop = document.querySelector("#toogleDrop");
+toogleDrop.addEventListener("click", function () {
+  const dropdownContent = document.getElementById("dropdown-content");
+  dropdownContent.classList.toggle("block");
+});
